028d219bdfebe5e5ca91272374b301664d6644d0
RicardoAD17 <ricardoalmada036@gmail.com>
2026-06-16 19:37:19 -0600

    .

--- src/main/java/mx/edu/uaa/resources/ComentarioResource.java
+++ src/main/java/mx/edu/uaa/resources/ComentarioResource.java
@@ -17,6 +17,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import java.util.List;
 
 import java.time.ZoneId;
 import java.util.ArrayList;
